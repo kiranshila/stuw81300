e286a59a011ee92415ea15a17c97d8df4695c6aa
Kiran Shila <me@kiranshila.com>
2022-02-16 16:06:41 -0800

    Generative tests to cover round trip register structuring

--- src/registers.rs
+++ src/registers.rs
@@ -110,7 +110,6 @@ register!(
     },
     flags:
     {
-        vco_calb_disable: 26,
     }
 );
 
@@ -167,7 +166,7 @@ register!(
     numbers:
     {
         vco_amp: (3,15),
-        ref_buf_mode: (2,8),
+        ref_buff_mode: (2,8),
         ld_prec: (3,3),
         ld_count: (3,0),
     },
@@ -211,7 +210,7 @@ register!(
         dithering: 26,
         en_autocal: 20,
         cal_temp_comp: 12,
-        cal_acc_en: 1,
+        cal_acc_en: 9,
     }
 );
 
@@ -272,17 +271,141 @@ register!(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use proptest::prelude::*;
 
-    #[test]
-    fn round_trip_st0() {
-        let st0 = ST0 {
-            vco_calb_disable: true,
-            cp_sel: 31,
-            pfd_del: 2,
-            n: 0x0000DEAD,
-        };
+    proptest! {
+        #[test]
+        fn round_trip_st0(n in 0u32..131071u32, cp_sel in 0u32..31u32, pfd_del in 0u32..3u32) {
+            let st0 = ST0 {
+                cp_sel,
+                pfd_del,
+                n,
+            };
+            let rt: ST0 = Into::<u32>::into(&st0).into();
+            assert_eq!(rt, st0);
+        }
 
-        let rt: ST0 = Into::<u32>::into(&st0).into();
-        assert_eq!(rt, st0);
+        #[test]
+        fn round_trip_st1(frac in 0u32..2097151u32,
+                          dbr: bool,
+                          rf1_out_pd: bool,
+                          man_calb_en: bool,
+                          pll_sel: bool,
+                          rf1_sel: bool) {
+            let st1 = ST1 {
+                frac,
+                dbr,
+                rf1_out_pd,
+                man_calb_en,
+                pll_sel,
+                rf1_sel,
+            };
+            let rt: ST1 = Into::<u32>::into(&st1).into();
+            assert_eq!(rt, st1);
+        }
+
+        #[test]
+        fn round_trip_st2(dbr: bool, rf2_out_pd: bool, modu in 2u32..2097151u32) {
+            let st2 = ST2 {
+                dbr,
+                rf2_out_pd,
+                modu,
+            };
+            let rt: ST2 = Into::<u32>::into(&st2).into();
+            assert_eq!(rt, st2);
+        }
+
+        #[test]
+        fn round_trip_st3(dbr: bool,
+                          pd: bool,
+                          cp_leak_x2: bool,
+                          cp_leak in 0u32..31u32,
+                          cp_leak_dir: bool,
+                          dnsplit_en: bool,
+                          pfd_del_mode in 0u32..3u32,
+                          ref_path_sel in 0u32..3u32,
+                          r in 1u32..8191u32) {
+            let st3 = ST3 {dbr,pd, cp_leak, pfd_del_mode, ref_path_sel, r, cp_leak_x2, cp_leak_dir, dnsplit_en };
+            let rt: ST3 = Into::<u32>::into(&st3).into();
+            assert_eq!(rt, st3);
+        }
+
+        #[test]
+        fn round_trip_st4(calb_3v3_mode1: bool,
+                          rf_out_3v3: bool,
+                          ext_vco_en: bool,
+                          vco_amp in 0u32..7u32,
+                          calb_3v3_mode0: bool,
+                          vcalb_mode: bool,
+                          kvco_comp_dis: bool,
+                          pfd_pol: bool,
+                          ref_buff_mode in 0u32..3u32,
+                          mute_lock_en: bool,
+                          ld_activelow: bool,
+                          ld_prec in 0u32..7u32,
+                          ld_count in 0u32..7u32) {
+            let st4 = ST4 { vco_amp, ref_buff_mode, ld_prec, ld_count, calb_3v3_mode1, rf_out_3v3, ext_vco_en, calb_3v3_mode0, vcalb_mode, kvco_comp_dis, pfd_pol, mute_lock_en, ld_activelow };
+            let rt: ST4 = Into::<u32>::into(&st4).into();
+            assert_eq!(rt, st4);
+        }
+
+        #[test]
+        fn round_trip_st5(rf2_outbuf_lp: bool,demux_lp: bool,ref_buff_lp: bool) {
+            let st5 = ST5 { rf2_outbuf_lp, demux_lp, ref_buff_lp };
+            let rt: ST5 = Into::<u32>::into(&st5).into();
+            assert_eq!(rt,st5);
+        }
+
+        #[test]
+        fn round_trip_st6(dithering: bool,
+                          dsm_order in 0u32..3u32,
+                          en_autocal: bool,
+                          cal_temp_comp: bool,
+                          prchg_del in 0u32..3u32,
+                          cal_acc_en: bool,
+                          cal_div in 1u32..511u32) {
+            let st6 = ST6 { dsm_order, prchg_del, cal_div, dithering, en_autocal, cal_temp_comp, cal_acc_en };
+            let rt: ST6 = Into::<u32>::into(&st6).into();
+            assert_eq!(rt,st6);
+        }
+
+        #[test]
+        fn round_trip_st7(ld_sdo_tristate: bool,
+                          ld_sdo_mode: bool,
+                          spi_data_out_disable: bool,
+                          cycle_slip_en: bool,
+                          fstlck_en: bool,
+                          cp_sel_fl in 0u32..31u32,
+                          fstlck_cnt in 2u32..8191u32) {
+            let st7 = ST7 { cp_sel_fl, fstlck_cnt, ld_sdo_tristate, ld_sdo_mode, spi_data_out_disable, cycle_slip_en, fstlck_en };
+            let rt: ST7 = Into::<u32>::into(&st7).into();
+            assert_eq!(rt,st7);
+        }
+
+        #[test]
+        fn round_trip_st8(pd_rf2_disable: bool, reg_vco_4v5_vout in 2u32..3u32) {
+            let st8 = ST8 { reg_vco_4v5_vout, pd_rf2_disable };
+            let rt: ST8 = Into::<u32>::into(&st8).into();
+            assert_eq!(rt,st8);
+        }
+
+        #[test]
+        fn round_trip_st10(vco_sel in 0u32..3u32,
+                           word in 0u32..31u32,
+                           reg_dig_startup: bool,
+                           reg_ref_startup: bool,
+                           reg_rf_startup: bool,
+                           reg_vco_startup: bool,
+                           reg_vco_4v5_startup: bool,
+                           reg_dig_ocp: bool,
+                           reg_ref_ocp: bool,
+                           reg_rf_ocp: bool,
+                           reg_vco_ocp: bool,
+                           reg_vco_4v5_ocp: bool,
+                           lock_det: bool) {
+            let st10 = ST10 { vco_sel, word, reg_dig_startup, reg_ref_startup, reg_rf_startup, reg_vco_startup, reg_vco_4v5_startup, reg_dig_ocp, reg_ref_ocp, reg_rf_ocp, reg_vco_ocp, reg_vco_4v5_ocp, lock_det };
+            let rt: ST10 = Into::<u32>::into(&st10).into();
+            assert_eq!(rt,st10);
+        }
     }
 }
